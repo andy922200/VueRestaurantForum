235eaa3a4112aa89ae5dec9db53e56cb643a11b9
andysml <andy922200@gmail.com>
2019-12-09 11:18:40 +0800

    finish until S4 U133

--- src/views/RestaurantsTop.vue
+++ src/views/RestaurantsTop.vue
@@ -2,357 +2,58 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <RestaurantsTopCards
+    <h1 class="mt-5">人氣餐廳</h1>
+    <hr />
+    <div
       v-for="restaurant in restaurants"
       :key="restaurant.id"
-      :initial-restaurant="restaurant"
-    />
+      class="card mb-3"
+      style="max-width: 540px;margin: auto;"
+    >
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <router-link :to="{name:'restaurant',params:{id:restaurant.id}}">
+            <img class="card-img" :src="restaurant.image" />
+          </router-link>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{{restaurant.name}}</h5>
+            <span class="badge badge-secondary">收藏數：{{restaurant.FavoriteCount}}</span>
+            <p class="card-text">{{restaurant.description}}</p>
+            <router-link
+              :to="{name:'restaurant',params:{id:restaurant.id}}"
+              class="btn btn-primary mr-2"
+            >Show</router-link>
+
+            <button
+              v-if="restaurant.isFavorited"
+              type="button"
+              class="btn btn-danger mr-2"
+              @click.stop.prevent="deleteFavorite(restaurant.id)"
+            >移除最愛</button>
+            <button
+              v-else
+              type="button"
+              class="btn btn-primary"
+              @click.stop.prevent="addFavorite(restaurant.id)"
+            >加到最愛</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import usersAPI from "./../apis/users";
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
 import NavTabs from "./../components/NavTabs";
-import RestaurantsTopCards from "./../components/RestaurantsTopCards";
-const dummyData = {
-  restaurants: [
-    {
-      id: 19,
-      name: "Dustin Lynch",
-      tel: "1-382-742-2344 x941",
-      address: "102 Johnson Camp",
-      opening_hours: "08:00",
-      description: "Veritatis quae ad id amet. Similique est suscipit.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=19.604747512748677",
-      viewCounts: 2,
-      createdAt: "2019-07-30T16:24:55.436Z",
-      updatedAt: "2019-09-07T16:22:46.020Z",
-      CategoryId: 5,
-      FavoritedUsers: [
-        {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y",
-          isAdmin: false,
-          image: "https://i.imgur.com/Q14p2le.jpg",
-          createdAt: "2019-07-30T16:24:55.204Z",
-          updatedAt: "2019-09-09T05:34:35.986Z",
-          Favorite: {
-            UserId: 2,
-            RestaurantId: 19,
-            createdAt: "2019-08-05T09:07:28.598Z",
-            updatedAt: "2019-08-05T09:07:28.598Z"
-          }
-        },
-        {
-          id: 1,
-          name: "root test",
-          email: "root@example.com",
-          password:
-            "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-          isAdmin: true,
-          image: "https://i.imgur.com/58ImzMM.png",
-          createdAt: "2019-07-30T16:24:54.983Z",
-          updatedAt: "2019-09-15T07:49:43.737Z",
-          Favorite: {
-            UserId: 1,
-            RestaurantId: 19,
-            createdAt: "2019-09-16T07:32:38.661Z",
-            updatedAt: "2019-09-16T07:32:38.661Z"
-          }
-        }
-      ],
-      isFavorited: true,
-      FavoriteCount: 2
-    },
-    {
-      id: 7,
-      name: "Melvin Flatley Sr.",
-      tel: "1-255-894-3279 x2314",
-      address: "138 Quinn Street",
-      opening_hours: "08:00",
-      description: "quos dolore tenetur",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=43.25768589113377",
-      viewCounts: 12,
-      createdAt: "2019-07-30T16:24:55.434Z",
-      updatedAt: "2019-10-03T09:29:40.805Z",
-      CategoryId: 1,
-      FavoritedUsers: [
-        {
-          id: 1,
-          name: "root test",
-          email: "root@example.com",
-          password:
-            "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-          isAdmin: true,
-          image: "https://i.imgur.com/58ImzMM.png",
-          createdAt: "2019-07-30T16:24:54.983Z",
-          updatedAt: "2019-09-15T07:49:43.737Z",
-          Favorite: {
-            UserId: 1,
-            RestaurantId: 7,
-            createdAt: "2019-08-18T05:50:25.235Z",
-            updatedAt: "2019-08-18T05:50:25.235Z"
-          }
-        }
-      ],
-      isFavorited: true,
-      FavoriteCount: 1
-    },
-    {
-      id: 46,
-      name: "Jasper Mueller",
-      tel: "254-159-2190",
-      address: "44890 Dominique Wall",
-      opening_hours: "08:00",
-      description: "dolorem ratione et",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=67.08831513935154",
-      viewCounts: 37,
-      createdAt: "2019-07-30T16:24:55.443Z",
-      updatedAt: "2019-09-15T08:50:04.423Z",
-      CategoryId: 2,
-      FavoritedUsers: [
-        {
-          id: 1,
-          name: "root test",
-          email: "root@example.com",
-          password:
-            "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-          isAdmin: true,
-          image: "https://i.imgur.com/58ImzMM.png",
-          createdAt: "2019-07-30T16:24:54.983Z",
-          updatedAt: "2019-09-15T07:49:43.737Z",
-          Favorite: {
-            UserId: 1,
-            RestaurantId: 46,
-            createdAt: "2019-09-16T07:29:15.848Z",
-            updatedAt: "2019-09-16T07:29:15.848Z"
-          }
-        }
-      ],
-      isFavorited: true,
-      FavoriteCount: 1
-    },
-    {
-      id: 40,
-      name: "Robyn Schroeder",
-      tel: "406.132.4400",
-      address: "647 Yundt Mountains",
-      opening_hours: "08:00",
-      description: "Incidunt rerum perspiciatis ut iste ipsum labore.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=15.902129223636363",
-      viewCounts: 11,
-      createdAt: "2019-07-30T16:24:55.441Z",
-      updatedAt: "2019-09-26T04:39:36.312Z",
-      CategoryId: 2,
-      FavoritedUsers: [
-        {
-          id: 1,
-          name: "root test",
-          email: "root@example.com",
-          password:
-            "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-          isAdmin: true,
-          image: "https://i.imgur.com/58ImzMM.png",
-          createdAt: "2019-07-30T16:24:54.983Z",
-          updatedAt: "2019-09-15T07:49:43.737Z",
-          Favorite: {
-            UserId: 1,
-            RestaurantId: 40,
-            createdAt: "2019-09-13T02:49:22.201Z",
-            updatedAt: "2019-09-13T02:49:22.201Z"
-          }
-        }
-      ],
-      isFavorited: true,
-      FavoriteCount: 1
-    },
-    {
-      id: 42,
-      name: "Melody Koepp",
-      tel: "(774) 342-2533",
-      address: "3318 Feeney Hollow",
-      opening_hours: "08:00",
-      description: "beatae iste alias",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=71.34426799459621",
-      viewCounts: 12,
-      createdAt: "2019-07-30T16:24:55.442Z",
-      updatedAt: "2019-09-15T08:47:06.852Z",
-      CategoryId: 2,
-      FavoritedUsers: [
-        {
-          id: 1,
-          name: "root test",
-          email: "root@example.com",
-          password:
-            "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-          isAdmin: true,
-          image: "https://i.imgur.com/58ImzMM.png",
-          createdAt: "2019-07-30T16:24:54.983Z",
-          updatedAt: "2019-09-15T07:49:43.737Z",
-          Favorite: {
-            UserId: 1,
-            RestaurantId: 42,
-            createdAt: "2019-09-13T02:49:16.478Z",
-            updatedAt: "2019-09-13T02:49:16.478Z"
-          }
-        }
-      ],
-      isFavorited: true,
-      FavoriteCount: 1
-    },
-    {
-      id: 11,
-      name: "Haley Trantow",
-      tel: "(919) 824-7063 x83066",
-      address: "86902 Yost Parks",
-      opening_hours: "08:00",
-      description: "Nam ipsum magnam quia.\r\nCulpa voluptate ducimus un",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=45.89406888977074",
-      viewCounts: 3,
-      createdAt: "2019-07-30T16:24:55.435Z",
-      updatedAt: "2019-10-07T09:04:42.057Z",
-      CategoryId: 5,
-      FavoritedUsers: [
-        {
-          id: 11,
-          name: "parag",
-          email: "p@mail.com",
-          password:
-            "$2a$10$a6Ce2r46Xd1iFF4EyXJu3uyCQE3jpEq2uy0ex.qG3ihcXMxmL0Tyu",
-          isAdmin: null,
-          image: null,
-          createdAt: "2019-10-07T09:03:32.408Z",
-          updatedAt: "2019-10-07T09:03:32.408Z",
-          Favorite: {
-            UserId: 11,
-            RestaurantId: 11,
-            createdAt: "2019-10-07T09:07:04.095Z",
-            updatedAt: "2019-10-07T09:07:04.095Z"
-          }
-        }
-      ],
-      isFavorited: false,
-      FavoriteCount: 1
-    },
-    {
-      id: 24,
-      name: "Al Von",
-      tel: "038-326-5052",
-      address: "5559 VonRueden Courts",
-      opening_hours: "08:00",
-      description: "Aliquid numquam hic. Minima perferendis quia cum t",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=37.9512586281334",
-      viewCounts: 33,
-      createdAt: "2019-07-30T16:24:55.438Z",
-      updatedAt: "2019-08-27T11:50:44.476Z",
-      CategoryId: 1,
-      FavoritedUsers: [
-        {
-          id: 1,
-          name: "root test",
-          email: "root@example.com",
-          password:
-            "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-          isAdmin: true,
-          image: "https://i.imgur.com/58ImzMM.png",
-          createdAt: "2019-07-30T16:24:54.983Z",
-          updatedAt: "2019-09-15T07:49:43.737Z",
-          Favorite: {
-            UserId: 1,
-            RestaurantId: 24,
-            createdAt: "2019-08-27T03:57:54.794Z",
-            updatedAt: "2019-08-27T03:57:54.794Z"
-          }
-        }
-      ],
-      isFavorited: true,
-      FavoriteCount: 1
-    },
-    {
-      id: 32,
-      name: "Eriberto Zboncak V",
-      tel: "(626) 794-3858 x2457",
-      address: "372 Reinger Shores",
-      opening_hours: "08:00",
-      description: "Veritatis non ratione temporibus impedit et id ess",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=48.817524543311094",
-      viewCounts: 21,
-      createdAt: "2019-07-30T16:24:55.440Z",
-      updatedAt: "2019-09-09T15:36:12.862Z",
-      CategoryId: 3,
-      FavoritedUsers: [
-        {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y",
-          isAdmin: false,
-          image: "https://i.imgur.com/Q14p2le.jpg",
-          createdAt: "2019-07-30T16:24:55.204Z",
-          updatedAt: "2019-09-09T05:34:35.986Z",
-          Favorite: {
-            UserId: 2,
-            RestaurantId: 32,
-            createdAt: "2019-08-05T09:07:29.998Z",
-            updatedAt: "2019-08-05T09:07:29.998Z"
-          }
-        }
-      ],
-      isFavorited: false,
-      FavoriteCount: 1
-    },
-    {
-      id: 26,
-      name: "Zoe Connelly",
-      tel: "724-565-0111",
-      address: "8718 Nolan Roads",
-      opening_hours: "08:00",
-      description: "Ipsum aspernatur quis est voluptates maiores quam ",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=51.62937107379864",
-      viewCounts: 27,
-      createdAt: "2019-07-30T16:24:55.438Z",
-      updatedAt: "2019-08-26T10:07:25.048Z",
-      CategoryId: 1,
-      FavoritedUsers: [],
-      isFavorited: false,
-      FavoriteCount: 0
-    },
-    {
-      id: 25,
-      name: "Austen Runte Jr.",
-      tel: "(111) 281-9830 x0839",
-      address: "76697 Greenfelder Isle",
-      opening_hours: "08:00",
-      description: "Incidunt voluptas quo. Et tempora facere totam odi",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=47.68607895620682",
-      viewCounts: 3,
-      createdAt: "2019-07-30T16:24:55.438Z",
-      updatedAt: "2019-08-27T10:12:55.028Z",
-      CategoryId: 3,
-      FavoritedUsers: [],
-      isFavorited: false,
-      FavoriteCount: 0
-    }
-  ]
-};
 
 export default {
   components: {
-    NavTabs,
-    RestaurantsTopCards
+    NavTabs
   },
   data() {
     return {
@@ -360,11 +61,76 @@ export default {
     };
   },
   created() {
-    this.fetchRestaurants();
+    this.fetchTopRestaurants();
   },
   methods: {
-    fetchRestaurants() {
-      this.restaurants = dummyData.restaurants;
+    async fetchTopRestaurants() {
+      try {
+        const { data, statusText } = await restaurantsAPI.getTopRestaurants();
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+        this.restaurants = data.restaurants;
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "無法取得熱門餐廳，請稍後再試"
+        });
+      }
+    },
+    async addFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.addFavorite({
+          restaurantId
+        });
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+        this.restaurants = this.restaurants
+          .map(restaurant => {
+            if (restaurant.id !== restaurantId) {
+              return restaurant;
+            }
+            return {
+              ...restaurant,
+              FavoriteCount: restaurant.FavoriteCount + 1,
+              isFavorited: true
+            };
+          })
+          .sort((a, b) => b.FavoriteCount - a.FavoriteCount);
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "無法加入餐廳最愛，請稍後再試"
+        });
+      }
+    },
+    async deleteFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFavorite({
+          restaurantId
+        });
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+        this.restaurants = this.restaurants
+          .map(restaurant => {
+            if (restaurant.id !== restaurantId) {
+              return restaurant;
+            }
+            return {
+              ...restaurant,
+              FavoriteCount: restaurant.FavoriteCount - 1,
+              isFavorited: false
+            };
+          })
+          .sort((a, b) => b.FavoriteCount - a.FavoriteCount);
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "無法移除餐廳最愛，請稍後再試"
+        });
+      }
     }
   }
 };
