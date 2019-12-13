e5752e03baea8766eaaa38c98cecc76963215ea4
andysml <andy922200@gmail.com>
2019-12-13 14:25:33 +0800

    finish until S4 U135

--- src/views/AdminRestaurantEdit.vue
+++ src/views/AdminRestaurantEdit.vue
@@ -1,35 +1,18 @@
 // ./src/components/AdminRestaurantEdit.vue
 <template>
   <div class="container py-5">
-    <AdminRestaurantForm :initial-restaurant="restaurant" @after-submit="handleAfterSubmit" />
+    <AdminRestaurantForm
+      :initial-restaurant="restaurant"
+      :is-processing="isProcessing"
+      @after-submit="handleAfterSubmit"
+    />
   </div>
 </template>
 
 <script>
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
 import AdminRestaurantForm from "./../components/AdminRestaurantForm.vue";
-
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: "Laurence Reynolds",
-    tel: "1-657-067-3756 x9782",
-    address: "187 Kirlin Squares",
-    opening_hours: "08:00",
-    description: "sit est mollitia",
-    image:
-      "https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887",
-    viewCounts: 1,
-    createdAt: "2019-07-30T16:24:55.432Z",
-    updatedAt: "2019-07-30T17:26:43.260Z",
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    }
-  }
-};
 
 export default {
   components: {
@@ -46,34 +29,72 @@ export default {
         description: "",
         image: "",
         openingHours: ""
-      }
+      },
+      isProcessing: false
     };
   },
   created() {
     const { id } = this.$route.params;
     this.fetchRestaurant(id);
   },
+  beforeRouteUpdate(to, from, next) {
+    const { id } = to.params;
+    this.fetchRestaurant(id);
+    next();
+  },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log("fetchRestaurant id:", restaurantId);
-      const { restaurant } = dummyData;
-      this.restaurant = {
-        ...this.restaurant,
-        id: restaurant.id,
-        name: restaurant.name,
-        categoryId: restaurant.CategoryId,
-        tel: restaurant.tel,
-        address: restaurant.address,
-        description: restaurant.description,
-        image: restaurant.image,
-        openingHours: restaurant.opening_hours
-      };
+    async fetchRestaurant(restaurantId) {
+      try {
+        const {
+          data: { restaurant },
+          statusText
+        } = await adminAPI.restaurants.getDetail({ restaurantId });
+
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+        this.restaurant = {
+          ...this.restaurant,
+          id: restaurant.id,
+          name: restaurant.name,
+          categoryId: restaurant.CategoryId,
+          tel: restaurant.tel,
+          address: restaurant.address,
+          description: restaurant.description,
+          image: restaurant.image,
+          openingHours: restaurant.opening_hours
+        };
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "無法取得餐廳資料，請稍後再試"
+        });
+      }
     },
-    handleAfterSubmit(formData) {
-      // 透過 API 將表單資料送到伺服器
+    async handleAfterSubmit(formData) {
+      try {
+        this.isProcessing = true;
+        const { data, statusText } = await adminAPI.restaurants.update({
+          restaurantId: this.restaurant.id,
+          formData
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        //發送完資料後跳轉
+        this.$router.push({ name: "admin-restaurants" });
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "無法建立/更新餐廳資料，請稍後再試"
+        });
+      }
+
+      /* 透過 API 將表單資料送到伺服器
       for (let [name, value] of formData.entries()) {
         console.log(name + ": " + value);
-      }
+      }*/
     }
   }
 };
